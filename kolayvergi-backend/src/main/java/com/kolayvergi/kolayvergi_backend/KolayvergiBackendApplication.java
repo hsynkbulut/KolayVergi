5b4d1482e0c032734fb0a678dd30283f7f8b6b04
hsynkbulut <hsyn.kbulut@gmail.com>
2025-04-16 10:25:11 +0300

    New project created

---
+++ kolayvergi-backend/src/main/java/com/kolayvergi/kolayvergi_backend/KolayvergiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kolayvergi.kolayvergi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolayvergiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KolayvergiBackendApplication.class, args);
+	}
+
+}
---
+++ kolayvergi-backend/src/test/java/com/kolayvergi/kolayvergi_backend/KolayvergiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolayvergi.kolayvergi_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolayvergiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
