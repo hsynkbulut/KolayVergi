183fd1f8fa7be07ec20aaa8b4035eb1f581d3299
hsynkbulut <hsyn.kbulut@gmail.com>
2025-04-21 15:53:17 +0300

    EntityScan, ComponentScan ve EnableJpaRepositories anotasyonları eklendi

--- kolayvergi-backend/src/main/java/com/kolayvergi/starter/KolayvergiBackendApplication.java
+++ kolayvergi-backend/src/main/java/com/kolayvergi/starter/KolayvergiBackendApplication.java
@@ -2,7 +2,13 @@ package com.kolayvergi.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan(basePackages = {"com.kolayvergi"})
+@ComponentScan(basePackages = {"com.kolayvergi"})
+@EnableJpaRepositories(basePackages = {"com.kolayvergi"})
 @SpringBootApplication
 public class KolayvergiBackendApplication {
 
