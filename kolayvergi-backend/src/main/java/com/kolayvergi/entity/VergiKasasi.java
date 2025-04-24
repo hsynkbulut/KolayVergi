f5b554ff90745ac43daf024e1b85ab0367177a3a
hsynkbulut <hsyn.kbulut@gmail.com>
2025-04-24 13:03:04 +0300

    VergiKasasi'nda enum'ın değeri DB'ye yazıldı, birkaç ufak düzeltme yapıldı

--- kolayvergi-backend/src/main/java/com/kolayvergi/entity/VergiKasasi.java
+++ kolayvergi-backend/src/main/java/com/kolayvergi/entity/VergiKasasi.java
@@ -2,22 +2,23 @@ package com.kolayvergi.entity;
 
 import com.kolayvergi.entity.base.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "vergi_kasasi")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class VergiKasasi extends BaseEntity {
 
+    @Enumerated(EnumType.STRING)
     private VergiTuru vergiTuru;
 
     private BigDecimal toplamBakiye;
--- kolayvergi-backend/src/main/java/com/kolayvergi/entity/base/BaseEntity.java
+++ kolayvergi-backend/src/main/java/com/kolayvergi/entity/base/BaseEntity.java
@@ -2,12 +2,14 @@ package com.kolayvergi.entity.base;
 
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-@Data
+@Getter
+@Setter
 public abstract class BaseEntity {
 
     @Id
